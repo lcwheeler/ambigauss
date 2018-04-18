9027063c6394be9de790a6613e43bd27f5e3e8c5
lcwheeler <lwheeler9@gmail.com>
2018-04-18 10:55:12 -0600

    add the Spectrum object and remove Dataset object

---
+++ ambigauss/spectrum.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from .fitter import residual, fit, bayes_fit
+from .curves import multigaussian, multilorentzian
+
+class Spectrum(object):
+    
+    """Object class that holds a data set, which can be imported from a data file."""
+    
+    def __init__(self):
+        """Instantiate an instance of the Spectrum class."""
+        self.r = None
+        self.xdata = None
+        self.ydata = None
+        self.xmodel = None
+        self.ymodel = None
+
+    def fit(xdata, ydata, fit): 
+	    """Use the specified fit function to fit a model to the data."""
+        
+        # Call fit function.
+        self.r = fit(xdata, ydata)
+
+        # Store the x and y data as arrays, attributes of the Spectrum object. 
+        self.xdata = np.array(xdata)
+        self.ydata = np.array(ydata)
+   
+    def plot_fit(self, curve):
+        """Function to generate a quick x vs. y plot of the Spectrum fit."""
+        
+        # Plot fitter results
+        self.xmodel = np.linspace(0,10, 1000)
+        self.ymodel = curve(xmodel, self.r.params)
+
+        plt.plot(self.xdata, self.ydata , '.')
+        plt.plot(self.xmodel, self.ymodel, '-')
+        parameters.pretty_print()
+        
+    
